88eb82ecb4349dd15f362c04a63ff32cffe67fb7
carlcrede <33420878+carlcrede@users.noreply.github.com>
2022-09-13 01:56:34 +0200

    no user selection

--- src/components/Item.tsx
+++ src/components/Item.tsx
@@ -3,10 +3,10 @@ import { MovieResult } from "../types/request-types";
 function Item(props: MovieResult) {
     const { media_type = 'Movie' } = props;
     return (
-        <div className="basis-1/6">
+        <div className="basis-1/6 select-none">
             <div className="overflow-hidden relative rounded-lg">
                 <img src={`https://image.tmdb.org/t/p/w500${props.poster_path}`} alt="%PUBLIC_URL%/img-placeholder.png" />
-                <span className="absolute bottom-2 right-3 text-xs select-none px-1 py-0.5 text-[#8a8d98] rounded-sm">{media_type}</span>
+                {/* <span className="absolute bottom-2 right-3 text-xs select-none px-1 py-0.5 text-[#8a8d98] rounded-sm">{media_type}</span> */}
             </div>
         </div>
     );
