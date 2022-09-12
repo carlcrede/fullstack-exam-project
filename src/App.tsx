163318e7c928b85dd3e0739195e153893edd6605
carlcrede <33420878+carlcrede@users.noreply.github.com>
2022-09-12 19:49:20 +0200

    bg fixed + added footer

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,14 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Footer from './components/Footer';
 import Header from './components/Header';
 import ItemsContainer from './components/ItemsContainer';
 
 function App() {
   return (
-    <div className="App bg-[#060D17] h-screen">
+    <div className="App bg-[#060D17]">
       <Header />
       <ItemsContainer />
+      <Footer />
     </div>
   );
 }
---
+++ src/components/Footer.tsx
@@ -0,0 +1,11 @@
+const Footer = () => {
+    return (
+        <div className="p-10 text-5xl font-extrabold select-none">
+            <span className="bg-clip-text text-transparent bg-gradient-to-r from-pink-500 to-violet-500">
+                This is the footer
+            </span>
+        </div>
+    )
+}
+
+export default Footer;
