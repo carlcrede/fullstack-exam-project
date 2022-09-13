77529051dcca801ae199abd08cb33ffae79d3395
Christian Hasselbalch <carl_hassel@hotmail.com>
2022-09-13 22:50:46 +0000

    styling bonanza

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,6 +1,8 @@
 const Footer = () => {
     return (
-        <div className="p-10 text-5xl font-extrabold select-none">
+        <div 
+            className="p-10 mt-10 text-5xl font-extrabold select-none sticky 
+                        bottom-0 bg-gradient-to-t from-[#060D17] via-[#060D17]/90 to-[#060D17]/10">
             <span className="bg-clip-text text-transparent bg-gradient-to-r from-pink-500 to-violet-500">
                 This is the footer
             </span>
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,10 +1,18 @@
+import React from "react";
+
 function Header() {
     return (
-        <div className="pt-4 mb-10 text-5xl font-extrabold select-none">
-            <span className="bg-clip-text text-transparent bg-gradient-to-r from-pink-500 to-violet-500">
-                This is the header
-            </span>
-        </div>
+        <React.Fragment>
+            <div 
+                className="py-10 mb-10 sticky top-0 z-10 bg-gradient-to-b from-[#060D17] via-[#060D17]/90 via-[#060D17]/40 to-[#060D17]/10">
+                <span 
+                    className="bg-clip-text text-transparent bg-gradient-to-r 
+                                from-pink-500 to-violet-500 text-5xl font-extrabold select-none"
+                                >
+                    CineSwipe
+                </span>
+            </div>
+        </React.Fragment>
     );
 }
 
