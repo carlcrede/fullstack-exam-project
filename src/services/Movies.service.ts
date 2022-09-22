7dc6c829998a70e39f1ecf3c6334a0ac33ff5b90
carlcrede <33420878+carlcrede@users.noreply.github.com>
2022-09-22 16:50:01 +0200

    added service methods for getting one item

--- src/services/Movies.service.ts
+++ src/services/Movies.service.ts
@@ -1,7 +1,8 @@
 import http from '../http-common';
-import { DiscoverMovieResponse, MovieResponse, ShowResponse } from '../types/request-types';
+import { MovieResponse } from '../types/request-types';
 class MovieDataService {
     getAll = async (page: number = 1) => await http.get<MovieResponse[]>(`/items/discover/movies?page=${page}`);
+    get = async (id: string) => await http.get<MovieResponse>(`/items/movies/${id}`);
 }
 
 export default new MovieDataService()
--- src/services/Tv.service.ts
+++ src/services/Tv.service.ts
@@ -1,7 +1,8 @@
 import http from '../http-common';
-import { DiscoverTvResponse, ShowResponse } from '../types/request-types';
+import { ShowResponse } from '../types/request-types';
 class TvDataService {
     getAll = async (page: number = 1) => await http.get<ShowResponse[]>(`/items/discover/tv?page=${page}`);
+    get = async (id: string) => await http.get<ShowResponse>(`/items/tv/${id}`);
 }
 
 export default new TvDataService()
