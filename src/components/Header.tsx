a5b38c6d62f47e8c9546f30d02540d3a335fc467
carlcrede <33420878+carlcrede@users.noreply.github.com>
2022-09-29 19:43:46 +0200

    header links to /

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,16 +1,19 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 function Header() {
     return (
         <React.Fragment>
             <div 
                 className="py-10 mb-10 sticky top-0 z-10 bg-gradient-to-b from-[#060D17] via-[#060D17]/90 via-[#060D17]/40 to-[#060D17]/10">
-                <span 
-                    className="bg-clip-text text-transparent bg-gradient-to-r 
-                                from-pink-500 to-violet-500 text-5xl font-extrabold select-none"
-                                >
-                    CineSwipe
-                </span>
+                <Link to={'/'}>
+                    <span 
+                        className="bg-clip-text text-transparent bg-gradient-to-r 
+                                    from-pink-500 to-violet-500 text-5xl font-extrabold select-none"
+                                    >
+                        CineSwipe
+                    </span>
+                </Link>
             </div>
         </React.Fragment>
     );
