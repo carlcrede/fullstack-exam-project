b2874b260422db3ac2d5d18bce72871a4b406b3a
carlcrede <33420878+carlcrede@users.noreply.github.com>
2022-09-29 19:44:39 +0200

    display 'All' as active by default

--- src/components/MediaButtons.tsx
+++ src/components/MediaButtons.tsx
@@ -1,11 +1,12 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 function MediaButtons({ onSelectMedia }: any) {
+    const [defaultSelected, setDefaultSelected] = useState(true);
     return (
         <div className="flex flex-row gap-3 text-gray-500">
             <div className='rounded-l-md'>
                 <button
-                    className='text-2xl  hover:text-white transition duration-500 focus:text-white focus:border-b-2'
+                    className={`text-2xl hover:text-white transition duration-500 focus:text-white focus:border-b-2 ${defaultSelected ? 'border-b-2': ''}`}
                     onClick={() => onSelectMedia({ movie: true, tv: true })}
                 >All
                 </button>
@@ -13,14 +14,14 @@ function MediaButtons({ onSelectMedia }: any) {
             <div className='rounded-l-md'>
                 <button
                     className='text-2xl hover:text-white transition duration-500 focus:text-white focus:border-b-2'
-                    onClick={() => onSelectMedia({ movie: true, tv: false })}
+                    onClick={() => {setDefaultSelected(false); onSelectMedia({ movie: true, tv: false })}}
                 >Movies
                 </button>
             </div>
             <div className='rounded-r-md'>
                 <button
                     className='text-2xl hover:text-white transition duration-500 focus:text-white focus:border-b-2'
-                    onClick={() => onSelectMedia({ movie: false, tv: true })}
+                    onClick={() => {setDefaultSelected(false); onSelectMedia({ movie: false, tv: true })}}
                 >Tv
                 </button>
             </div>
