241d04c1120bda6a6b9ed07962fc7b6b27fd9c31
mstrucka <mstrucka@gmail.com>
2022-09-29 13:52:38 +0200

    router navigation improved

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { Route, Routes } from 'react-router-dom';
+import {Link, Outlet, Route, Routes} from 'react-router-dom';
 import './App.css';
 import Footer from './components/Footer';
 import Header from './components/Header';
@@ -9,16 +9,20 @@ import Login from "./login";
 import Signup from "./register";
 
 function App() {
+
   return (
     <div className="App">
       <Header />
-      <Routes>
-        <Route path={'/login'} element={<Login />} />
-        <Route path={'/signup'} element={<Signup />} />
-        <Route path={'/'} element={<ItemsContainer />} />
-        <Route path={'tv/:id'} element={<TvDetails />} />
-        <Route path={'movie/:id'} element={<MovieDetails />} />
-      </Routes>
+        <nav
+            style={{
+                borderBottom: "solid 1px",
+                paddingBottom: "1rem",
+            }}
+        >
+            <Link to="/login">Login</Link> |{" "}
+            <Link to="/signup">Register</Link>
+        </nav>
+        <Outlet/>
       <Footer />
     </div>
   );
--- src/index.tsx
+++ src/index.tsx
@@ -3,7 +3,12 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { BrowserRouter as Router } from 'react-router-dom';
+import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
+import Login from "./login";
+import Signup from "./register";
+import ItemsContainer from "./components/ItemsContainer";
+import TvDetails from "./components/TvDetails";
+import MovieDetails from "./components/MovieDetails";
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
@@ -11,7 +16,25 @@ const root = ReactDOM.createRoot(
 root.render(
   <React.StrictMode>
     <Router>
-      <App />
+        <Routes>
+            // TODO
+
+            <Route path={'/'} element={<App />}>
+                <Route path={'/'} element={<ItemsContainer />}/>
+                <Route path={'tv/:id'} element={<TvDetails />} />
+                <Route path={'movie/:id'} element={<MovieDetails />} />
+                <Route path={'login'} element={<Login />} />
+                <Route path={'signup'} element={<Signup />} />
+                <Route
+                path="*"
+                element={
+                    <main style={{ padding: "1rem" }}>
+                        <p>There's nothing here!</p>
+                    </main>
+                }
+                />
+            </Route>
+        </Routes>
     </Router>
   </React.StrictMode>
 );
