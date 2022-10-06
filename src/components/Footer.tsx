cc55800003b7020d9406fd19a74526993c53eb8d
carlcrede <33420878+carlcrede@users.noreply.github.com>
2022-10-06 03:56:15 +0200

    fixed header and footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,11 +1,13 @@
 const Footer = () => {
     return (
-        <div 
-            className="p-10 mt-10 text-5xl font-extrabold select-none sticky 
-                        bottom-0 bg-gradient-to-t from-[#060D17] via-[#060D17]/90 to-[#060D17]/10">
-            <span className="bg-clip-text text-transparent bg-gradient-to-r from-pink-500 to-violet-500">
-                This is the footer
-            </span>
+        <div className="flex flex-row justify-center">
+            <div 
+                className="w-screen p-10 mt-10 text-5xl font-extrabold select-none fixed
+                            bottom-0 bg-gradient-to-t from-[#060D17] via-[#060D17]/90 to-[#060D17]/10">
+                <span className="bg-clip-text text-transparent bg-gradient-to-r from-pink-500 to-violet-500">
+                    This is the footer
+                </span>
+            </div>
         </div>
     )
 }
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,19 +1,22 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import Navbar from "../navbar";
 
 function Header() {
     return (
         <React.Fragment>
-            <div 
-                className="py-10 mb-10 sticky top-0 z-10 bg-gradient-to-b from-[#060D17] via-[#060D17]/90 via-[#060D17]/40 to-[#060D17]/10">
-                <Link to={'/'}>
-                    <span 
-                        className="bg-clip-text text-transparent bg-gradient-to-r 
-                                    from-pink-500 to-violet-500 text-5xl font-extrabold select-none"
-                                    >
-                        CineSwipe
-                    </span>
-                </Link>
+            <div className="flex flex-row justify-center gap-4 z-40 py-10 bg-gradient-to-b min-h-fit sticky top-0 from-[#060D17] via-[#060D17]/90 via-[#060D17]/40 to-[#060D17]/10">
+                <div>
+                    <Link to={'/'}>
+                        <span 
+                            className="bg-clip-text text-transparent bg-gradient-to-r 
+                                        from-pink-500 to-violet-500 background-animate text-5xl font-extrabold select-none"
+                                        >
+                            CineSwipe
+                        </span>
+                    </Link>
+                </div>
+                <Navbar />
             </div>
         </React.Fragment>
     );
