51881455ebe0d979f2f727ab2d7d4c0ab9f274f3
carlcrede <33420878+carlcrede@users.noreply.github.com>
2022-10-06 03:56:38 +0200

    fixed token + styling

--- src/components/Filters.tsx
+++ src/components/Filters.tsx
@@ -4,7 +4,7 @@ import MediaButtons from './MediaButtons'
 
 const Filters: React.FunctionComponent<any> = ({ onSelectMedia }) => {
   return (
-    <div className="flex flex-row my-5 gap-5 justify-start">
+    <div className="flex flex-row my-5 gap-5 justify-center">
         <MediaButtons onSelectMedia={onSelectMedia} />
         <FilterOptions />
     </div>
--- src/login.tsx
+++ src/login.tsx
@@ -4,38 +4,39 @@ import {useNavigate} from "react-router-dom";
 import useToken from "./useToken";
 
 export default function Login() {
-    const { setToken } = useToken();
+    const { setToken, token } = useToken();
     const navigate = useNavigate();
     const [password, setPassword] = useState("")
     const [user, setUser] = useState("")
 
     const handleSubmit = async (e: { preventDefault: () => void; }) => {
         e.preventDefault();
-        await AuthService.login(user, password).then(r => setToken(r.data['token']));
-        navigate('/')
+        await AuthService.login(user, password).then(r => {
+            setToken(r.data['token']);
+            navigate('/', { state: { token: r.data['token'] } });
+        });
     }
 
     return (
-        <div className=" min-h-screen bg-gray-50 flex flex-col justify-center">
-            <div className="max-w-md w-full mx-auto">
-                <div className="text-3xl mt-2 font-bold text-gray-900 text-center"> Login</div>
-            </div>
-            <div className="max-w-md w-full mx-auto mt-4 bg-white p-8 border border-gray-300">
+        <div className="flex flex-row justify-center">
+            <div className="max-w-md w-full mx-auto mt-4 bg-white p-8 border border-gray-300 rounded-lg text-gray-900">
+                <div className="text-3xl font-bold text-center mb-4">Login</div>
                 <form action="" className="space-y-6" onSubmit={handleSubmit}>
                     <div>
-                        <label className="text-sm font-bold text-gray-600 block">Username or Email</label>
+                        <label className="text-sm font-bold block">Username or Email</label>
                         <input name="user" type="text" onChange={e => setUser(e.target.value)}
-                               className="w-full p-2 border border-gray-300 rounded mt-1 text-gray-600"
+                               className="w-full p-2 border border-gray-300 rounded mt-1"
                                placeholder="Enter Username or Email" autoComplete="off" />
                     </div>
                     <div>
-                        <label className="text-sm font-bold text-gray-600 block">Password</label>
+                        <label className="text-sm font-bold block">Password</label>
                         <input name="password" type="password" onChange={e => setPassword(e.target.value)}
-                               className="w-full p-2 border border-gray-300 rounded mt-1 text-gray-600"
+                               className="w-full p-2 border border-gray-300 rounded mt-1"
                                placeholder="Enter Password" autoComplete="off"/>
                     </div>
-                    <div>
-                        <button className="w-full bg-blue-600 rounded text-white">Submit</button>
+                    <div className="bg-gradient-to-r 
+                                    from-pink-500 to-violet-500 rounded-lg p-2">
+                        <button className="w-full rounded text-white font-bold select-none">Log in</button>
                     </div>
                 </form>
             </div>
