0c166d98e40d07ac64b89a714896916110b5008f
carlcrede <33420878+carlcrede@users.noreply.github.com>
2022-10-25 01:55:40 +0200

    removed a bunch of unused stuff

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,11 @@ import { Outlet } from 'react-router-dom';
 import './App.css';
 import Footer from './components/Footer';
 import Header from './components/Header';
-import Navbar from "./navbar";
 
 function App() {
   return (
     <div className="App mx-10 min-h-screen">
       <Header />
-      {/* <Navbar/> */}
       <Outlet/>
       <Footer/>
     </div>
