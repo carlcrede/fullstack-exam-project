988fcc0f2c753ee211daec7cda2a5a2532ca7eea
carlcrede <33420878+carlcrede@users.noreply.github.com>
2022-10-25 02:02:24 +0200

    footer centered text

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,6 +1,6 @@
 const Footer = () => {
     return (
-        <div className="flex flex-row justify-center">
+        <div className="flex flex-row justify-center text-center">
             <div 
                 className="w-screen p-10 mt-10 text-5xl font-extrabold select-none fixed
                             bottom-0 bg-gradient-to-t from-[#060D17] via-[#060D17]/90 to-[#060D17]/10">
