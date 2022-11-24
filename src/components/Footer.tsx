5d43820927d35cea3302009f45e3591e2d9da758
mstrucka <mstrucka@gmail.com>
2022-11-24 21:01:00 +0100

    WIP trending

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,9 +1,22 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import {Link} from 'react-router-dom';
 import bg from '../assets/footer-bg.jpg';
 import logo from '../assets/cineswipe.png';
+import {TrendingResponse} from "../types/request-types";
+import moviesService from "../services/Movies.service";
 
 const Footer = () => {
+    const [movies, setMovies] = useState<TrendingResponse>();
+
+
+    useEffect(() => {
+        moviesService.getTrending().then((response) => {
+            setMovies(response.data);
+            console.log(response.data)
+            console.log(response.data)
+        })
+    }, [])
+
     return (
         <div className="py-24 px-8 bg-top bg-cover bg-no-repeat sticky top-full" style={{backgroundImage: `url(${bg})`}}>
             {/*TODO: I deleted container and no screen width here, works on my 15' laptop and 24' screen, try it out, also change logo?*/}
@@ -16,8 +29,14 @@ const Footer = () => {
                 <div className="grid grid-cols-3">
                     <div className="flex flex-col justify-center items-center mt-1 font-bold">
                         <Link to="/">Home</Link>
+                        {
+                            movies?.results?.slice(0, 5).map((movie, i) => (
+                                <span key={i} className="py-2 px-6 border-solid border-red-400 border-2 text-xs font-bold rounded-[30px]">{movie.title}</span>
+                            ))
+                        }
                     </div>
                     <div className="flex flex-col justify-center items-center mt-1 font-bold">
+                        <Link to="/">Home</Link>
                         <Link to="https://www.rottentomatoes.com/">Rotten Tomatoes</Link>
                     </div>
                     <div className="flex flex-col justify-center items-center mt-1 font-bold">
--- src/services/Movies.service.ts
+++ src/services/Movies.service.ts
@@ -1,8 +1,9 @@
 import http from '../http-common';
-import {CreditsResponse, MovieResponse, VideosResponse} from '../types/request-types';
+import {CreditsResponse, MovieResponse, TrendingResponse, VideosResponse} from '../types/request-types';
 class MovieDataService {
     getAll = async (page: number = 1) => await http.get<MovieResponse[]>(`/items/discover/movies?page=${page}`);
     get = async (id: string) => await http.get<MovieResponse & {videos: VideosResponse} & {credits: CreditsResponse}>(`/items/movie/${id}`);
+    getTrending = async () => await http.get<TrendingResponse>(`/trending/movie/day`);
 }
 
 export default new MovieDataService();
