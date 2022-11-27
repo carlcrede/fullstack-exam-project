5ad52499e15f044dac8d6b30b4c1b2d2ecd0f484
carlcrede <33420878+carlcrede@users.noreply.github.com>
2022-11-28 00:20:37 +0100

    user can change email and username

--- src/components/user/UserProfile.tsx
+++ src/components/user/UserProfile.tsx
@@ -1,20 +1,47 @@
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import UserService from "../../services/User.service"
 
 type User = {
     username: string;
     email: string;
+    profilePicture: string;
 }
 
 const UserProfile = () => {
     const [user, setUser] = useState<User | null>(null);
+    const [email, setEmail] = useState<string>('');
+    const [username, setUsername] = useState<string>('');
+    const [error, setError] = useState<string>('');
 
     useEffect(() => {
         UserService.getUserProfile().then((response) => {
             setUser(response.data);
+            setEmail(response.data.email);
+            setUsername(response.data.username);
         });
     }, []);
     
+    const saveInfo = (e: { preventDefault: () => void; }) => {
+        e.preventDefault();        
+        UserService.updateUserProfile({email, username}).then(
+            (response) => {
+                setUser(prev => ({...prev, ...response.data}));
+                setEmail(response.data.email);
+                setUsername(response.data.username);
+            },
+            (error) => {
+                console.log(error);
+                setError(error.response.data);
+            }
+        );
+    }
+
+    const handleProfilePictureUpload = (e: { target: { files: any; }; }) => {
+        const [file] = e.target.files;
+        if (file) {
+            console.log(file);
+        }
+    }
 
   return (
     <>
@@ -24,23 +51,25 @@ const UserProfile = () => {
         <div className="grid grid-cols-3 justify-items-center gap-4 bg-[#282c34] p-5 rounded-md">
             <div className="bg-[#060D17]/50 bg-cover rounded-md p-4 w-full">
                 <div className="flex">
-                    <div className="basis-1/2">    
+                    <div className="basis-1/2">  
                         <div className="text-2xl">My Information</div>
                         <div className="text-lg">Email</div>
-                        <input className="bg-[#282c34] rounded-md p-2 border-[1px]" type="text" placeholder={user?.email} />
+                        <input className="bg-[#282c34] rounded-md p-2 border-[1px]" type="email" placeholder={user?.email} onChange={e => setEmail(e.target.value)} />
                         <div className="text-lg">Username</div>
-                        <input className="bg-[#282c34] rounded-md p-2 border-[1px]" type="text" placeholder={user?.username}/>
-                        <div className="">
-                            <button className="p-2 mt-5 bg-gradient-to-r from-pink-500 to-violet-500 background-animate rounded-lg text-lg">Save changes</button>
-                        </div>
+                        <input className="bg-[#282c34] rounded-md p-2 border-[1px]" type="text" placeholder={user?.username} onChange={e => setUsername(e.target.value)}/>
+                        {error.length > 0 && (
+                            <div className="border border-t-0 border-red-400 rounded-b bg-red-100 px-2 py-2 text-red-700 ">{error}</div>
+                        )}
+                        <button onClick={saveInfo} className="p-2 mt-5 bg-gradient-to-r from-pink-500 to-violet-500 background-animate rounded-lg text-lg">Save changes</button>
                     </div>
                     <div className="basis-1/2">
                         <div className="text-2xl">Profile picture</div>
-                        <img className="h-16 w-16 object-cover rounded-full" src="https://images.unsplash.com/photo-1580489944761-15a19d654956?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1361&q=80" alt="" />
-                        <input type="file" className="block w-full text-sm text-slate-500 
-                            file:px-4 file:rounded-full file:border-0 file:text-sm
-                            
-                        "/>
+                        <img className="w-20 h-20 object-cover rounded-full" src={user?.profilePicture} alt="" />
+                        <input type="file" accept="image/*" multiple={false} 
+                            className="block w-full text-sm text-slate-500 
+                            file:px-4 file:rounded-full file:border-0 file:text-sm"
+                            onChange={handleProfilePictureUpload}
+                        />
                     </div>
                 </div>
             </div>
--- src/services/User.service.ts
+++ src/services/User.service.ts
@@ -3,6 +3,7 @@ import http from "../http-common";
 
 class UserService {
     getUserProfile = async () => await http.get(`/me`, { headers: { Authorization: `Bearer ${getAuthToken()}` } });
+    updateUserProfile = async (data: any) => await http.put(`/me`, data, { headers: { Authorization: `Bearer ${getAuthToken()}` } });
 }
 
 export default new UserService();
