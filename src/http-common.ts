b70c0e6cd0fad8841850756650797a01be2d86c9
carlcrede <33420878+carlcrede@users.noreply.github.com>
2022-12-01 14:48:38 +0100

    env var typo

--- src/http-common.ts
+++ src/http-common.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export default axios.create({
-    baseURL: import.meta.env.PRODUCTION ? 'https://fullstack-backend-s4bv.onrender.com': 'http://localhost:8080',
+    baseURL: import.meta.env.PROD ? 'https://fullstack-backend-s4bv.onrender.com': 'http://localhost:8080',
     headers: {
         'Content-type': 'application/json',
     },
