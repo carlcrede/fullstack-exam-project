06338ade81902552e47d0f2e443823c1a3c10c7f
carlcrede <33420878+carlcrede@users.noreply.github.com>
2022-12-07 12:07:32 +0100

    added toasts

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import { useEffect, useState } from 'react';
+import { Toaster } from 'react-hot-toast';
 import { Outlet } from 'react-router-dom';
 import './App.css';
 import { getAuthToken } from './components/auth/Auth';
@@ -15,6 +16,18 @@ const App = () => {
 
   return (
     <div className="App mx-10 min-h-screen">
+      <Toaster
+        position='top-center'
+        reverseOrder={false}
+        toastOptions={{
+          style: {
+            borderRadius: '10px',
+            background: '#333',
+            color: '#fff',
+            padding: '16px',
+          },
+        }}
+      />
       <Header user={user} />
       <Outlet/>
       <Footer/>
--- src/components/user/UserProfile.tsx
+++ src/components/user/UserProfile.tsx
@@ -1,11 +1,16 @@
 import { useEffect, useState } from "react";
+import toast from "react-hot-toast";
 import { useNavigate } from "react-router-dom";
 import UserService from "../../services/User.service"
 
-type User = {
+export type User = {
     username: string;
     email: string;
     profilePicture: string;
+    favorites: {
+        movieDbId: string;
+        media_type: 'movie' | 'tv';
+    }[];
 }
 
 const UserProfile = () => {
@@ -26,18 +31,23 @@ const UserProfile = () => {
             }
         );
     }, []);
-    
+
     const saveInfo = (e: { preventDefault: () => void; }) => {
-        e.preventDefault();        
-        UserService.updateUserProfile({email, username}).then(
-            (response) => {
-                setUser(prev => ({...prev, ...response.data}));
-                setEmail(response.data.email);
-                setUsername(response.data.username);
+        e.preventDefault();
+        toast.promise(
+            UserService.updateUserProfile({ email, username }),
+            {
+                loading: 'Updating profile...',
+                success: (res) => {
+                    setUser(prev => ({ ...prev, ...res.data }));
+                    setEmail(res.data.email);
+                    setUsername(res.data.username);
+                    return <b>Profile updated!</b> 
+                },
+                error: (err) => {
+                    return <b>Failed to update profile. {err.response.data}</b>
+                },
             },
-            (error) => {
-                setError(error.response.data);
-            }
         );
     }
 
@@ -46,52 +56,56 @@ const UserProfile = () => {
         if (file) {
             const formData = new FormData();
             formData.append('profilePicture', file);
-            UserService.uploadProfilePicture(formData).then(
-                (response) => {
-                    setUser(prev => ({...prev, ...response.data}));
+            toast.promise(
+                UserService.uploadProfilePicture(formData),
+                {
+                    loading: 'Uploading profile picture...',
+                    success: (res) => {
+                        setUser(prev => ({ ...prev, ...res.data }));
+                        return <b>Profile picture uploaded!</b>
+                    },
+                    error: (err) => <b>Failed to upload profile picture. {err.response.data}</b>,
                 },
-                (error) => {
-                    setError(error.response.data);
-                }
-        )}
+            );
+        }
     }
 
-  return (
-    <>
-        <div className="flex justify-center">
-            <div className="text-5xl">Hi, {user?.username}!</div>
-        </div>
-        <div className="grid grid-cols-3 justify-items-center gap-4 bg-[#282c34] p-5 rounded-md">
-            <div className="bg-[#060D17]/50 bg-cover rounded-md p-4 w-full">
-                <div className="flex">
-                    <div className="basis-1/2">  
-                        <div className="text-2xl">My Information</div>
-                        <div className="text-lg">Email</div>
-                        <input className="bg-[#282c34] rounded-md p-2 border-[1px]" type="email" placeholder={user?.email} onChange={e => setEmail(e.target.value)} />
-                        <div className="text-lg">Username</div>
-                        <input className="bg-[#282c34] rounded-md p-2 border-[1px]" type="text" placeholder={user?.username} onChange={e => setUsername(e.target.value)}/>
-                        {error.length > 0 && (
-                            <div className="border border-t-0 border-red-400 rounded-b bg-red-100 px-2 py-2 text-red-700 ">{error}</div>
-                        )}
-                        <button onClick={saveInfo} className="p-2 mt-5 bg-gradient-to-r from-pink-500 to-violet-500 background-animate rounded-lg text-lg">Save changes</button>
-                    </div>
-                    <div className="basis-1/2">
-                        <div className="text-2xl">Profile picture</div>
-                        <img className="w-20 h-20 object-cover rounded-full" src={user?.profilePicture} alt="" />
-                        <input type="file" accept="image/*" multiple={false} 
-                            className="block w-full text-sm text-slate-500 
+    return (
+        <>
+            <div className="flex justify-center">
+                <div className="text-5xl">Hi, {user?.username}!</div>
+            </div>
+            <div className="grid grid-cols-3 justify-items-center gap-4 bg-[#282c34] p-5 rounded-md">
+                <div className="bg-[#060D17]/50 bg-cover rounded-md p-4 w-full">
+                    <div className="flex">
+                        <div className="basis-1/2">
+                            <div className="text-2xl">My Information</div>
+                            <div className="text-lg">Email</div>
+                            <input className="bg-[#282c34] rounded-md p-2 border-[1px]" type="email" placeholder={user?.email} onChange={e => setEmail(e.target.value)} />
+                            <div className="text-lg">Username</div>
+                            <input className="bg-[#282c34] rounded-md p-2 border-[1px]" type="text" placeholder={user?.username} onChange={e => setUsername(e.target.value)} />
+                            {error.length > 0 && (
+                                <div className="border border-t-0 border-red-400 rounded-b bg-red-100 px-2 py-2 text-red-700 ">{error}</div>
+                            )}
+                            <button onClick={saveInfo} className="p-2 mt-5 bg-gradient-to-r from-pink-500 to-violet-500 background-animate rounded-lg text-lg">Save changes</button>
+                        </div>
+                        <div className="basis-1/2">
+                            <div className="text-2xl">Profile picture</div>
+                            <img className="w-20 h-20 object-cover rounded-full" src={user?.profilePicture} alt="" />
+                            <input type="file" accept="image/*" multiple={false}
+                                className="block w-full text-sm text-slate-500 
                             file:px-4 file:rounded-full file:border-0 file:text-sm"
-                            onChange={handleProfilePictureUpload}
-                        />
+                                onChange={handleProfilePictureUpload}
+                            />
+                        </div>
                     </div>
                 </div>
+                <div className="col-span-2 bg-[#060D17]/50 w-full p-4 rounded-md">
+                    <div className="text-2xl">My Favorites</div>
+                </div>
             </div>
-            <div className="col-span-2 bg-[#060D17]/50 w-full p-4 rounded-md">
-                <div className="text-2xl">My Favorites</div>
-            </div>
-        </div>
-    </>
-  )
+        </>
+    )
 };
 
 export default UserProfile;
